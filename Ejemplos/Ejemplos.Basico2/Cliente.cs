a08213331a25137f73b9b35ccdd2da661d94edf4
Titiushko <titiushko@gmail.com>
2019-03-20 14:39:23 -0600

    :minidisc: Propiedades Autoimplementadas Son una característica incluida a partir de la versión 3.0 de C# que hacen más concisa la declaración de una propiedad. Es posible aplicarla siempre que no exista “lógica” en los descriptores de acceso de la propiedad.

---
+++ Ejemplos/Ejemplos.Basico2/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace Ejemplos.Basico2
+{
+    public class Cliente
+    {
+        public int IdCliente { get; private set; }
+        public string Nombre { get; set; }
+
+        public Cliente()
+        {
+            Nombre = "Vacio";
+            IdCliente = 33;
+        }
+    }
+}
---
+++ Ejemplos/Ejemplos.Basico2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejemplos.Basico2
+{
+    class Program
+    {
+        /// <summary>
+        /// Propiedades Autoimplementadas
+        /// Son una característica incluida a partir de la versión 3.0 de C# que hacen más concisa la declaración de una propiedad.
+        /// Es posible aplicarla siempre que no exista “lógica” en los descriptores de acceso de la propiedad.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Cliente c1 = new Cliente();
+            Console.WriteLine(c1.Nombre);
+            Console.WriteLine(c1.IdCliente.ToString());
+            c1.Nombre = "Pepe";
+            //c1.IdCliente = 66; daría error de compilación porque es private.
+
+            Console.WriteLine(c1.Nombre);
+            Console.ReadLine();
+        }
+    }
+}
