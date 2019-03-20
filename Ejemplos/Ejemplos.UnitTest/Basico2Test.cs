d767a27b2057bd83dd261a1593b554c9357affc0
Titiushko <titiushko@gmail.com>
2019-03-20 15:19:12 -0600

    :alien: UnitTest para probar la clase Cliente del proyecto Ejemplos.Basico2

---
+++ Ejemplos/Ejemplos.UnitTest/Basico2Test.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ejemplos.Basico2;
+
+namespace Ejemplos.UnitTest
+{
+    [TestClass]
+    public class Basico2Test
+    {
+        private const string NOMBRE = "Pepe";
+        private const string NOMBRE_VACIO = "Vacio";
+
+        /// <summary>
+        /// UnitTest para probar la clase Cliente del proyecto Ejemplos.Basico2
+        /// </summary>
+        [TestMethod]
+        public void Basico2Test_ClaseBasica()
+        {
+            Cliente vCliente = new Cliente();
+
+            Assert.IsNotNull(vCliente.IdCliente);
+            Assert.AreEqual(vCliente.Nombre, NOMBRE_VACIO);
+            Assert.AreNotEqual(vCliente.Nombre, NOMBRE);
+
+            vCliente.Nombre = NOMBRE;
+
+            Assert.AreEqual(vCliente.Nombre, NOMBRE);
+            Assert.AreNotEqual(vCliente.Nombre, NOMBRE_VACIO);
+        }
+    }
+}
