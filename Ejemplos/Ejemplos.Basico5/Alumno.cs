b439608387b0533057a256007aa1aed916c98362
Titiushko <titiushko@gmail.com>
2019-03-20 14:43:04 -0600

    :apple: Miembros Estáticos (static) o Compartidos Se trata de una especie de campo común a todos los objetos de la clase. El siguiente ejemplo muestra como poder llevar un contador de los objetos Alumnos creados, para ello crearemos el campo “contador” y la propiedad “Contador” ambas con la propiedad “static”. Los miembros compartidos o estático son campos, propiedades o métodos a los que pueden acceder todas las instancias de una clase. Bien muy bien cuando cuando queremos gestionar datos que no son específicos de una instancia de clase sino de la propia clase.

---
+++ Ejemplos/Ejemplos.Basico5/Alumno.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Ejemplos.Basico5
+{
+    public class Alumno
+    {
+        private static int contador;
+        private int edad;
+        private string nombre;
+        private int matricula;
+
+        public static int Contador
+        {
+            get { return contador; }
+        }
+
+        public Alumno() // Constructor por defecto
+        {
+            edad = 0;
+            contador = ++contador;
+            matricula = contador;
+        }
+
+        public Alumno(string p_nombre, int p_edad)
+        {
+            edad = p_edad;
+            nombre = p_nombre;
+            contador = ++contador;
+            matricula = contador;
+        }
+
+        public int Edad
+        {
+            get
+            {
+                return edad;
+            }
+            set
+            {
+                edad = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre != null ? nombre : "Sin nombre"; // condición ? cierto : falso
+            }
+        }
+
+        public void Imprime()
+        {
+            Console.WriteLine("Nro.:" + matricula.ToString() + " de " + Contador.ToString() + " | Alumno: " + this.Nombre + " - Edad: " + this.Edad.ToString() + " años.");
+        }
+    }
+}
---
+++ Ejemplos/Ejemplos.Basico5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ejemplos.Basico5
+{
+    class Program
+    {
+        /// <summary>
+        /// Miembros Estáticos (static) o Compartidos
+        /// Se trata de una especie de campo común a todos los objetos de la clase.
+        /// El siguiente ejemplo muestra como poder llevar un contador de los objetos Alumnos creados, para ello crearemos el campo “contador” y la propiedad
+        /// “Contador” ambas con la propiedad “static”. Los miembros compartidos o estático son campos, propiedades o métodos a los que pueden acceder todas
+        /// las instancias de una clase.
+        /// Bien muy bien cuando cuando queremos gestionar datos que no son específicos de una instancia de clase sino de la propia clase.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Total alumnos antes:" + Alumno.Contador.ToString());
+            Alumno a = new Alumno();
+            Alumno b = new Alumno("Pedro", 33);
+            Console.WriteLine("Total alumnos después:" + Alumno.Contador.ToString());
+            a.Imprime();
+            b.Imprime();
+            Console.ReadLine();
+        }
+    }
+}
