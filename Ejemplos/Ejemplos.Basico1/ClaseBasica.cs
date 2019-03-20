47d35a22930397b0821522e1c15eab346a0a6ce5
Titiushko <titiushko@gmail.com>
2019-03-20 14:35:46 -0600

    :dragon_face: Ejercicios de programación orientada a objetos: Clases y objetos

---
+++ Ejemplos/Ejemplos.Basico1/ClaseBasica.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ejemplos.Basico1
+{
+    public class ClaseBasica
+    {
+        public int i = 10; //Atributo
+        private int tantoporcien; //Atributo
+
+        public int TantoPorCien //Propiedad
+        {
+            get { return tantoporcien; }
+            set
+            {
+                if ((value >= 0) && (value <= 100))
+                {
+                    tantoporcien = value;
+                }
+            }
+        }
+
+        public void Resetea() //Método
+        {
+            tantoporcien = 0;
+        }
+
+        public string Mensaje()
+        {
+            return this.i.ToString() + " - " + this.tantoporcien.ToString() + "%";
+        }
+
+        public void Imprime()
+        {
+            Console.WriteLine(Mensaje());
+        }
+    }
+}
---
+++ Ejemplos/Ejemplos.Basico1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejemplos.Basico1
+{
+    class Program
+    {
+        /// <summary>
+        /// Comparar cómo hemos creado el objeto “a” y cómo el objeto “b“, el resultado es el mismo pero el objeto “b” fue creado directamente
+        /// a partir de la clase “ClaseBasica” mientras que el objeto “a” fue creado a partir del objeto “Objeto“.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            object Objeto;
+            Objeto = new ClaseBasica();
+            ClaseBasica a;
+            a = (ClaseBasica)Objeto;
+            a.i += a.i; // Expresion lambda. Equivalente a "a.i = a.i + a.i";
+            a.TantoPorCien = 70;
+
+            a.Imprime();
+            a.Resetea();
+            a.Imprime();
+
+            ClaseBasica b = new ClaseBasica();
+            b.i = +b.i; // ojo esto simplemente asigna el signo del entero es decir no hace nada en este caso;
+            b.TantoPorCien = 75;
+            b.TantoPorCien = 750;
+            b.Imprime();
+
+            Console.ReadLine();
+        }
+    }
+}
