4d67e8dacc55650317605e16c03e343f9dd555f2
Titiushko <titiushko@gmail.com>
2019-03-20 14:41:08 -0600

    :card_index: Constructores Son métodos que llevan el mismo nombre que la clase y se ejecutan automáticamente cuando se crea el objeto. Al igual que otros métodos pueden estar sobrecargados.Veamos un ejemplo de una clase “Alumno” con dos campos, un constructor sobrecargado y un método. No devuelven ningún tipo, ni siquiera “void”. Nunca se le puede llamar de manera explícita en el código, únicamente implícitamente en la creación de una instancia de la clase.El constructor de una clase que no espera parámetro alguno es designado como “constructor por defecto” de la clase. Normalmente un constructor se encarga de la inicialización de campos.

---
+++ Ejemplos/Ejemplos.Basico3/Alumno.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Ejemplos.Basico3
+{
+    public class Alumno
+    {
+        private int edad;
+        private string nombre;
+
+        public Alumno() // Constructor por defecto
+        {
+            edad = 0;
+
+        }
+
+        public Alumno(string p_nombre, int p_edad)
+        {
+            edad = p_edad;
+            nombre = p_nombre;
+        }
+
+        public int Edad
+        {
+            get
+            {
+                return edad;
+            }
+            set
+            {
+                edad = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre != null ? nombre : "Sin nombre";
+            }
+        }
+
+        public void Imprime()
+        {
+            Console.WriteLine("Alumno: " + this.Nombre + " - Edad: " + this.Edad.ToString() + " años.");
+        }
+    }
+}
---
+++ Ejemplos/Ejemplos.Basico3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejemplos.Basico3
+{
+    class Program
+    {
+        /// <summary>
+        /// Constructores
+        /// Son métodos que llevan el mismo nombre que la clase y se ejecutan automáticamente cuando se crea el objeto.
+        /// Al igual que otros métodos pueden estar sobrecargados.Veamos un ejemplo de una clase “Alumno” con dos campos, un constructor sobrecargado y un método.
+        /// No devuelven ningún tipo, ni siquiera “void”. Nunca se le puede llamar de manera explícita en el código, únicamente implícitamente en la creación
+        /// de una instancia de la clase.El constructor de una clase que no espera parámetro alguno es designado como “constructor por defecto” de la clase.
+        /// Normalmente un constructor se encarga de la inicialización de campos.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno();
+            Alumno b = new Alumno("Pedro", 33);
+
+            a.Imprime();
+            b.Imprime();
+            Console.ReadLine();
+        }
+    }
+}
